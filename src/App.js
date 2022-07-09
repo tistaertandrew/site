a919d3a5c7f4cb05c396840a59507e5c3c7e3044
Andrew Tistaert <a.tistaert@student.helmo.be>
2022-07-09 19:22:36 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [name, setName] = useState('Andrew')
+    const names = ['Ludovic', 'Noureddine', 'Andrew']
+
+    const pick_up_name = () => {
+        setInterval(() => {
+            let index = Math.round(Math.random()*(names.length-1))
+            setName(names[index])
+        }, 5000)
+    }
+
+    pick_up_name()
+
+    return (
+        <div>
+            <h1>{name}</h1>
+            <h2>Work in progress...</h2>
+        </div>
+    );
 }
 
 export default App;
