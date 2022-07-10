a74e22a12c8b1d5b6079db2c36d376941c9e7f88
Andrew Tistaert <a.tistaert@student.helmo.be>
2022-07-10 19:52:06 +0200

    WorkInProgress centré

--- src/components/WorkInProgress.js
+++ src/components/WorkInProgress.js
@@ -2,7 +2,7 @@ import sablier from "../assets/hourglass.png";
 
 function WorkInProgress(props) {
     return (
-        <div>
+        <div className="center-div">
             <h1>Work in progress..</h1>
             <img className="App-logo" src={sablier} alt='coucou'/>
         </div>
