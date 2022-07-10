e27c869f37082904e46307adc163fa5a13b2c04a
Andrew Tistaert <a.tistaert@student.helmo.be>
2022-07-10 19:36:01 +0200

    ajout component WorkInProgress

--- src/App.js
+++ src/App.js
@@ -1,26 +1,8 @@
-import {useState} from "react";
-import sablier from './assets/hourglass.png'
+import WorkInProgress from "./components/WorkInProgress";
 
 function App() {
-    const [name, setName] = useState('Andrew')
-    const [rotation, setRotation] = useState(0)
-    const names = ['Ludovic', 'Noureddine', 'Andrew']
-
-    const pick_up_name = () => {
-        setInterval(() => {
-            let index = Math.round(Math.random()*(names.length-1))
-            setName(names[index])
-        }, 5000)
-    }
-
-    pick_up_name()
-
     return (
-        <div>
-            <h1>{name}</h1>
-            <span>Work in progress </span>
-            <img className="App-logo" src={sablier} alt='coucou'/>
-        </div>
+        <WorkInProgress />
     );
 }
 
---
+++ src/components/WorkInProgress.js
@@ -0,0 +1,13 @@
+import sablier from "../assets/hourglass.png";
+
+function WorkInProgress(props) {
+    return (
+        <div>
+            <h1>Work in progress..</h1>
+            <img className="App-logo" src={sablier} alt='coucou'/>
+        </div>
+    )
+
+}
+
+export default WorkInProgress;
